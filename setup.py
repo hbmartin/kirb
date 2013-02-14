976af2771091f0086e95eeabd5b26a6ad431ebd8
Harold Martin <harold.martin@gmail.com>
2013-02-13 22:17:21 -0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+NAME='kirb'
+setup(name=NAME,
+      version='1.0.1',
+      license='BSD',
+      url='https://github.com/hbmartin/kirb',
+      description='Continuous Integration & ReBuild',
+      author='Harold Martin',
+      author_email='harold.martin@openx.com',
+      packages=find_packages(),
+      long_description=open('README.txt').read(),
+      package_data = { '': ['*.txt', '*.rst'] },
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Documentation',
+          'Topic :: Utilities',
+      ],
+      platforms='any',
+      namespace_packages=[NAME],
+      install_requires = ['docutils>=0.3','watchdog>=0.6']
+     )
+
